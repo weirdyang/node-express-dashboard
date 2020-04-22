d2a95b15851c53c3637304aaa3129501d992ec7a
bitglimpse <bitglimpse@gmail.com>
2020-04-22 07:40:23 -0600

    Oops, add services

---
+++ services/select-file-service.js
@@ -0,0 +1,11 @@
+function getDirectoryContents(files, currentDir, query) {
+}
+
+function isDirectory(currentDir, file) {
+}
+
+function readDir(currentDir, res, query) {
+}
+
+exports.get = (req, res) => {
+};
