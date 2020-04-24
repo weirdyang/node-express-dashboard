7b0d514d86f31ffda8c3d2dfa9e7aff5ba4b5407
bitglimpse <bitglimpse@gmail.com>
2020-04-24 11:33:12 -0600

    add initial files

---
+++ services/settings-service.js
@@ -0,0 +1,19 @@
+function getSettings() {
+  return {}
+}
+
+function writeSettings(newSettings) {
+}
+
+function getDefaultDir() {
+}
+
+function isValidDir(dirPath) {
+}
+
+module.exports = {
+  getSettings,
+  writeSettings,
+  getDefaultDir,
+  isValidDir
+};
