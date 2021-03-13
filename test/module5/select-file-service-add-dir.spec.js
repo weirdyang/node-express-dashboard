9eb6fb3dca8862db051e52b16a1109cd328d26ed
weirdyang <wy4786@gmail.com>
2021-03-14 03:21:25 +0800

    completed adding websockets

--- test/module5/select-file-service-add-dir.spec.js
+++ test/module5/select-file-service-add-dir.spec.js
@@ -1,16 +1,21 @@
 describe('services/select-file-service.js', () => {
-  const source = fs.readFileSync(path.join(process.cwd(), 'services/select-file-service.js'), 'utf8');
+  const source = fs.readFileSync(
+    path.join(process.cwd(), 'services/select-file-service.js'),
+    'utf8',
+  );
   const selectFileService = jscs(source);
 
   it('should set `dir` global @select-file-service-add-dir', () => {
-    const dir = selectFileService.findVariable("dir")
+    const dir = selectFileService.findVariable('dir');
 
     const dirMatch = {
-      "init.callee.object.name": "process",
-      "init.callee.property.name" : "cwd",
-    }
+      'init.callee.object.name': 'process',
+      'init.callee.property.name': 'cwd',
+    };
 
-    assert(dir.length && matchObj(dir, dirMatch),
-      "Are you assigning a global `const` named `dir` to `process.cwd()`?")
+    assert(
+      dir.length && matchObj(dir, dirMatch),
+      'Are you assigning a global `const` named `dir` to `process.cwd()`?',
+    );
   });
 });
